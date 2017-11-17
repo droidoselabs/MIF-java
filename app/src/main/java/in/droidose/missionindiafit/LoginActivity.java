10dd3450344f0e76183c9105d18c57705b74fbbc
Rajat Dhamija <rajat.dhamija@mail.click-labs.com>
2017-11-17 15:56:26 +0530

    feature_login: Login screen design changed

--- app/src/main/java/in/droidose/missionindiafit/LoginActivity.java
+++ app/src/main/java/in/droidose/missionindiafit/LoginActivity.java
@@ -4,20 +4,27 @@ import android.os.Bundle;
 import android.view.View;
 
 public class LoginActivity extends BaseActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initViews();
+        listeners();
     }
 
     private void initViews() {
+    }
+
+    private void listeners() {
     }
 
     @Override
     public void onClick(View view) {
         super.onClick(view);
         switch (view.getId()) {
+            default:
+                break;
         }
     }
 }
