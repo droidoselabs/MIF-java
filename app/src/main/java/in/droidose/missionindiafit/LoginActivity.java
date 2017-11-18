7e840018478b5fca9aefb1d89ccb3536e9c5059a
Rajat Dhamija <rajat.dhamija@mail.click-labs.com>
2017-11-18 19:56:42 +0530

    feature_login: Base code mvp login

--- app/src/main/java/in/droidose/missionindiafit/LoginActivity.java
+++ app/src/main/java/in/droidose/missionindiafit/LoginActivity.java
@@ -2,27 +2,46 @@ package in.droidose.missionindiafit;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class LoginActivity extends BaseActivity {
+    @BindView(R.id.etEmail)
+    EditText etEmail;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+    @BindView(R.id.tvSignup)
+    TextView tvSignup;
+    @BindView(R.id.tvForgotPassword)
+    TextView tvForgotPassword;
+    @BindView(R.id.btnSignIn)
+    EditText btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        initViews();
         listeners();
     }
 
-    private void initViews() {
-    }
-
     private void listeners() {
+        tvSignup.setOnClickListener(this);
+        tvForgotPassword.setOnClickListener(this);
+        btnSignIn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         super.onClick(view);
         switch (view.getId()) {
+            case R.id.tvSignup:
+                break;
+            case R.id.tvForgotPassword:
+                break;
+            case R.id.btnSignIn:
+                break;
             default:
                 break;
         }
